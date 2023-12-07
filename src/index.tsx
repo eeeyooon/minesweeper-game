e307ca2521167800a4ea6cfbf29b22147756eeeb
jiyoon <vywns9978@gmail.com>
2023-12-07 16:19:43 +0900

    setting: redux 설치 및 기초 코드 설정

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,17 @@ import App from './App';
 import { GlobalStyles } from './style/global';
 import { defaultTheme } from './style/theme';
 import { ThemeProvider } from 'styled-components';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<React.StrictMode>
-		<ThemeProvider theme={defaultTheme}>
-			<GlobalStyles />
-			<App />
-		</ThemeProvider>
+		<Provider store={store}>
+			<ThemeProvider theme={defaultTheme}>
+				<GlobalStyles />
+				<App />
+			</ThemeProvider>
+		</Provider>
 	</React.StrictMode>,
 );
